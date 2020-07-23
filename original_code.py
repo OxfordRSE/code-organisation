02925834f7e66894397560608e64fba90b136ec7
Graham Lee <graham.lee@cs.ox.ac.uk>
2020-07-23 17:18:18 +0100

    Fix formatting and patch up line numbers

--- original_code.py
+++ original_code.py
@@ -37,7 +37,8 @@ def deriv(x, t, u, alpha, beta, gamma):
 
 t = np.linspace(0, 210, 210)
 x_initial = s_initial, e_initial, i_initial, r_initial
-s, e, i, r = odeint(deriv, x_initial, t, args=(u_social_distancing, alpha, beta, gamma)).T
+s, e, i, r = odeint(deriv, x_initial, t, \
+    args=(u_social_distancing, alpha, beta, gamma)).T
 s0, e0, i0, r0 = odeint(deriv, x_initial, t, args=(0, alpha, beta, gamma)).T
 
 # plot the data
@@ -48,7 +49,8 @@ ax = [fig.add_subplot(311, axisbelow=True),
 pal = sns.color_palette()
 
 ax[0].stackplot(t/7, N*s, N*e, N*i, N*r, colors=pal, alpha=0.6)
-ax[0].set_title('Susceptible and Recovered Populations with {0:3.0f}% Effective Social Distancing'.format(u_social_distancing))
+ax[0].set_title('Susceptible and Recovered Populations\
+     with {0:3.0f}% Effective Social Distancing'.format(u_social_distancing))
 ax[0].set_xlabel('Weeks following Initial Campus Exposure')
 ax[0].set_xlim(0, t[-1]/7)
 ax[0].set_ylim(0, N)
@@ -58,7 +60,8 @@ ax[0].legend([
     'Infectious/symptomatic',
     'Recovered'], 
     loc='best')
-ax[0].plot(np.array([t_social_distancing, t_social_distancing]), ax[0].get_ylim(), 'r', lw=3)
+ax[0].plot(np.array([t_social_distancing, t_social_distancing]), \
+    ax[0].get_ylim(), 'r', lw=3)
 ax[0].plot(np.array([0, t[-1]])/7, [N/R0, N/R0], lw=3, label='herd immunity')
 ax[0].annotate("Start of social distancing",
     (t_social_distancing, 0), (t_social_distancing + 1.5, N/10),
@@ -69,7 +72,8 @@ ax[0].annotate("Herd Immunity without social distancing",
 
 ax[1].stackplot(t/7, N*i0,N*e0, colors=pal[2:0:-1], alpha=0.5)
 ax[1].stackplot(t/7, N*i, N*e, colors=pal[2:0:-1], alpha=0.5)
-ax[1].set_title('Infected Population with no Social Distancing and with {0:3.0f}% Effective Social Distancing'.format(u_social_distancing))
+ax[1].set_title('Infected Population with no Social Distancing\
+     and with {0:3.0f}% Effective Social Distancing'.format(u_social_distancing))
 ax[1].set_xlim(0, t[-1]/7)
 ax[1].set_ylim(0, max(0.3*N, 1.05*max(N*(e + i))))
 ax[1].set_xlabel('Weeks following Initial Campus Exposure')
@@ -77,7 +81,8 @@ ax[1].legend([
     'Infective/Symptomatic', 
     'Exposed/Not Sympotomatic'],
     loc='upper right')
-ax[1].plot(np.array([t_social_distancing, t_social_distancing]), ax[0].get_ylim(), 'r', lw=3)
+ax[1].plot(np.array([t_social_distancing, t_social_distancing]), \
+    ax[0].get_ylim(), 'r', lw=3)
 
 y0 = N*(e0 + i0)
 k0 = np.argmax(y0)
@@ -85,7 +90,8 @@ ax[1].annotate("No social distancing", (t[k0]/7, y0[k0] + 100))
 
 y = N*(e + i)
 k = np.argmax(y)
-ax[1].annotate("With {0:3.0f}% effective social distancing ".format(u_social_distancing), (t[k]/7, y[k] + 100))
+ax[1].annotate("With {0:3.0f}% effective social distancing ".format(u_social_distancing),\
+     (t[k]/7, y[k] + 100))
 
 for a in ax:
     a.xaxis.set_major_locator(plt.MultipleLocator(5))
